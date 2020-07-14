3cd7addf964a93b4694a1d4d58d8ee3d2f89fd86
frank <>
2020-07-14 15:55:36 +1000

    :sparkles: try build 2020/07/14

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 const HomePage = (): JSX.Element => {
-    return <div>This is the Homepageaaa</div>;
+    return <div>This is the Homepageaachangea</div>;
 };
 
 export default HomePage;
